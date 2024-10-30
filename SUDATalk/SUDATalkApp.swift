09547c698301f12502cf8ea8c6f13e9bd81e0e3e
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-10-30 19:54:07 +0900

    init: 프로젝트 초기 세팅

---
+++ SUDATalk/SUDATalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUDATalkApp.swift
+//  SUDATalk
+//
+//  Created by 김수경 on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SUDATalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
