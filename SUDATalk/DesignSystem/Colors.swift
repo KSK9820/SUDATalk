66809a66721bdc5863a835e762726a1e3bee79c2
dahyun <dahyun4477@gmail.com>
2024-11-01 22:12:52 +0900

    design: TextStyle, Colors, Images를 enum으로 DesignSystem 정의

---
+++ SUDATalk/DesignSystem/Colors.swift
@@ -0,0 +1,27 @@
+//
+//  Colors.swift
+//  SUDATalk
+//
+//  Created by 박다현 on 11/1/24.
+//
+
+import SwiftUI
+
+enum Colors {
+    static let primary = Color(red: 67, green: 97, blue: 238)
+    static let error = Color(red: 233, green: 102, blue: 108)
+    static let inactive = Color(red: 215, green: 215, blue: 215)
+    
+    static let black = Color(.black)
+    static let gray = Color(red: 221, green: 221, blue: 221)
+    static let white = Color(.white)
+    
+    static let textprimary = Color(red: 28, green: 28, blue: 28)
+    static let textSecondary = Color(red: 96, green: 96, blue: 96)
+    
+    static let bgPrimary = Color(red: 246, green: 246, blue: 246)
+    static let bgSecondary = Color(.white)
+    
+    static let Seperator = Color(red: 236, green: 236, blue: 236)
+    static let Alpha = Color(red: 128, green: 128, blue: 128)
+}
---
+++ SUDATalk/DesignSystem/Images.swift
@@ -0,0 +1,28 @@
+//
+//  Images.swift
+//  SUDATalk
+//
+//  Created by 박다현 on 11/1/24.
+//
+
+import SwiftUI
+
+enum Images {
+    static let notFound = Image("not_found")
+    static let readyToMake = Image("ready_to_make")
+    static let newMessage = Image("new_message")
+    static let close = Image("close")
+    static let email = Image("email")
+    static let help = Image("help")
+    static let more = Image("more")
+    static let plus = Image("plus")
+    static let tag = Image("tag")
+    static let chevronUp = Image("chevron_up")
+    static let chevronRight = Image("chevron_right")
+    static let chevronLeft = Image("chevron_left")
+    static let chevronDown = Image("chevron_down")
+    static let message = Image("message")
+    static let profile = Image("profile")
+    static let setting = Image("setting")
+    static let home = Image("home")
+}
