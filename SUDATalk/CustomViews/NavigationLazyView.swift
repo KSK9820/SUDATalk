44cac704aca80768f7e11696e742d493e00c165e
dahyun <dahyun4477@gmail.com>
2024-11-02 18:54:26 +0900

    feat: 초기화 지연을 위한 CustomVew 구현

--- SUDATalk/CustomViews/NavigationLazyView.swift
+++ SUDATalk/CustomViews/NavigationLazyView.swift
@@ -5,4 +5,17 @@
 //  Created by 박다현 on 11/2/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct NavigationLazyView<Content: View>: View {
+    
+    let build: () -> Content
+    
+    var body: some View {
+        build()
+    }
+    
+    init(_ build: @autoclosure @escaping () -> Content) {
+        self.build = build
+    }
+}
