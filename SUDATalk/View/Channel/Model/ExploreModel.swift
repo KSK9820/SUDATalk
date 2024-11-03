6975c4f223b2aa89741fcda54e2b3cf152a5b4ba
dahyun <dahyun4477@gmail.com>
2024-11-03 23:01:08 +0900

    refactor: NetworkManager 인스턴스를 전역 변수로 변경하여 재사용하도록 개선

--- SUDATalk/View/Channel/Model/ExploreModel.swift
+++ SUDATalk/View/Channel/Model/ExploreModel.swift
@@ -10,6 +10,7 @@ import Foundation
 
 final class ExploreModel: ObservableObject, ExploreModelStateProtocol {
     var cancellables: Set<AnyCancellable> = []
+    let networkManager = NetworkManager(dataTaskServices: DataTaskServices(), decodedServices: DecodedServices())
     
     @Published var channelList: [ChannelList] = []
     @Published var showAlert: Bool = false
@@ -21,10 +22,8 @@ extension ExploreModel: ExploreActionsProtocol {
             let requestChannel = try ChannelRouter.channel(param: workspaceID).makeRequest()
             let requestMyChannel = try ChannelRouter.myChannel(param: workspaceID).makeRequest()
             
-            let requestChannelPublisher = NetworkManager(dataTaskServices: DataTaskServices(), decodedServices: DecodedServices())
-                .fetchDecodedData(requestChannel, model: [ExploreResponse].self)
-            let requestMyChannelPublisher = NetworkManager(dataTaskServices: DataTaskServices(), decodedServices: DecodedServices())
-                .fetchDecodedData(requestMyChannel, model: [ExploreResponse].self)
+            let requestChannelPublisher = networkManager.fetchDecodedData(requestChannel, model: [ExploreResponse].self)
+            let requestMyChannelPublisher = networkManager.fetchDecodedData(requestMyChannel, model: [ExploreResponse].self)
             
             requestChannelPublisher
                 .zip(requestMyChannelPublisher)
