daebfca8a352aa2c417ddcd8e50912e0a95622e6
dahyun <dahyun4477@gmail.com>
2024-12-02 21:42:14 +0900

    refactor: 채널 삭제시 이동하는 rootView를 HomeView로 변경

--- SUDATalk/View/Channel/Setting/ChannelSettingView.swift
+++ SUDATalk/View/Channel/Setting/ChannelSettingView.swift
@@ -65,7 +65,8 @@ struct ChannelSettingView: View {
         }
         .onChange(of: container.model.goToList) { newValue in
             if newValue {
-                setRootView(what: LoginView())
+                let workspace = WorkSpacePresentationModel(workspaceID: SampleTest.workspaceID, name: "워크스페이스닷", coverImage: Images.help, ownerID: "", createdAt: Date())
+                setRootView(what: HomeView.build(workspace))
                }
         }
         .onChange(of: container.model.selectedSheet) { newValue in
